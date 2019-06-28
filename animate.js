7549a21a6f85c37afe27034ec4ed46af167e93fd
ashchu <ashchu890@gmail.com>
2019-06-27 19:23:24 -0700

    third commit

--- animate.js
+++ animate.js
@@ -2,7 +2,7 @@
 function changeText()
   {
     var sayings = ["CORRECT", "true, dogs are awesome", "there's only one right answer", "you're wrong sorry bud", "not today mista", "think again..", "try again...", "yes yes yes", "wohoo!! 10 points to you :D", "yes!!!"];
-    var rand = (Math.random() * 10);
+    var rand = (Math.random() * 9);
     rand = Math.round(rand);
     $('.response').html(sayings[rand]+ "");
   }
